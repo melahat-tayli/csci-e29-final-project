d4f507c454927484788979e437943909a989dab5
melahat-tayli <sahinmelahat@hotmail.com>
2020-12-13 20:13:29 -0800

    feat: apply precommit hooks to cli.py

--- final_project/cli.py
+++ final_project/cli.py
@@ -1,11 +1,15 @@
 from luigi import build
-from .test_model import TestModel
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
+
+from .test_model import TestModel
 
 
 def main():
     build(
-        [TestModel(model=RandomForestClassifier, source_test='test'), TestModel(model=LogisticRegression, source_test='test')],
+        [
+            TestModel(model=RandomForestClassifier, source_test="test"),
+            TestModel(model=LogisticRegression, source_test="test"),
+        ],
         local_scheduler=True,
     )
