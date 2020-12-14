3c62de18e282a1bbd64f7a9f64cc70315e1c3342
melahat-tayli <sahinmelahat@hotmail.com>
2020-12-13 21:41:41 -0800

    feat: add DownloadRawData to build to download data to local for visalization

--- final_project/cli.py
+++ final_project/cli.py
@@ -2,12 +2,14 @@ from luigi import build
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
+from .load_data import DownloadRawData
 from .test_model import TestModel
 
 
 def main():
     build(
         [
+            DownloadRawData(),
             TestModel(model=RandomForestClassifier, source_test="test"),
             TestModel(model=LogisticRegression, source_test="test"),
         ],
