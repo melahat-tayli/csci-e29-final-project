3fbaba461f358d09f02d523a9e7627a8f43a63f0
melahat-tayli <sahinmelahat@hotmail.com>
2020-12-14 23:06:45 -0800

    feat: add UploadRawData to Luigi build

--- final_project/cli.py
+++ final_project/cli.py
@@ -2,13 +2,14 @@ from luigi import build
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 
-from .load_data import DownloadRawData
-from .test_model import TestModel
+from .load_data import DownloadRawData, UploadRawData
+from .testperformance_model import TestModel
 
 
 def main():
     build(
         [
+            UploadRawData(),
             DownloadRawData(),
             TestModel(model=RandomForestClassifier, source_test="test"),
             TestModel(model=LogisticRegression, source_test="test"),
